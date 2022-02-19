6d423a7f7591f0604962326507245e6829cbd76d
ishelly1997 <ishelly1997@gmail.com>
2022-02-19 12:44:12 -0500

    added prompts, and formatted sample readme to be generated

---
+++ index.js
@@ -0,0 +1,62 @@
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+var fs = require('fs')
+
+var generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'Project Title'
+
+    },
+    {
+        name: 'projDesc',
+        type: 'input',
+        message: 'Project Description'
+    },
+    {
+        name:'installInst',
+        type:'',
+        message:'Installation Instructions'
+    },
+    {
+        name:'license',
+        type:'list',
+        choices:['MIT','more licenses']
+    },
+    {
+        name: 'gitHub',
+        type: 'input',
+        message: 'Enter your GitHub username'
+    },
+    {
+        name:'email',
+        type:'input',
+        message:'enter your email address'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    return fs.writeFileSync('sampleReadMe.md',data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    console.log(answers)
+writeToFile(generateMarkdown(answers))
+})
+  .catch((error) => {
+    if (error) {
+        console.log(error)
+    }
+  });
+}
+
+// Function call to initialize app
+init();
---
+++ sampleReadMe.md
@@ -0,0 +1,10 @@
+# title
+## Project Description
+undefined
+## Installation Instructions
+inst
+# License
+MIT
+# Contact Information
+ishelly
+ishelly
