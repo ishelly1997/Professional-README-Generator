8ab8cf25680f35f03f44257eb9b585f143268bd6
ishelly1997 <ishelly1997@gmail.com>
2022-02-20 20:04:59 -0500

    save work'

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,8 +7,10 @@ function renderLicenseBadge(license) {
     renderedLicense = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   } else if (licenseType === 'GPL') {
     renderedLicense = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (licenseType === 'Apache') {
+    renderedLicense = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   } else {
-    licenseType = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    licenseType = 'N/A'
   }
   console.log(licenseType);
   return renderedLicense;
@@ -20,33 +22,27 @@ function renderLicenseLink(license) {
   const licenseType = license.license;
   const renderedLicense = ''
 if (licenseType === 'MIT') {
-  renderedLicense = `[![License: MIT](https://opensource.org/licenses/MIT)`
+  renderedLink = `[![License: MIT](https://opensource.org/licenses/MIT)`
 } else if (licenseType === 'GPL') {
-  renderedLicense = `[![License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)`
+  renderedLink = `[![License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)`
 } else {
-  licenseType = `[![License](https://opensource.org/licenses/Apache-2.0)`
+  renderedLink = `[![License](https://opensource.org/licenses/Apache-2.0)`
 }
-console.log(renderedLicense);
+console.log(renderedLink);
 return renderedLicense;
 };
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  return `#License
-  ${data.license} `
-}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-${renderLicenseBadge(data.license)}
+${renderedLicense}
 ## Project Description
 ${data.projDescr}
 ## Installation Instructions
 ${data.installInst}
-${renderLicenseSection(data.license)}
-${data.license}
-${renderLicenseLink(data.license)}
+##License
+${licenseType}
+${renderedLink}
 ## Contact Information
 ${data.gitHub}
 ${data.email}
